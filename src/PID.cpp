4d036a484a27fabd5bfc2aeefb17122724ce958c
Nicholas Moellers <nmperson@gmail.com>
2017-07-11 19:50:30 -0700

    Implemented basic PID

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,29 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	this->Kp = -1.0*Kp;
+	this->Ki = -1.0*Ki;
+	this->Kd = -1.0*Kd;
+
+	d_error = 0;
+	p_error = 0;
+	i_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+
+	d_error = cte - p_error;
+	p_error = cte;
+	i_error = i_error + cte;
+
 }
 
 double PID::TotalError() {
+	return i_error;
+}
+
+double PID::Steer() {
+	double steer = Kp * p_error + Kd * d_error + Ki * i_error;
+	return steer;
 }
 
